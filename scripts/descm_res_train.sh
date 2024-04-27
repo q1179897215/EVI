26bccf87cdd990e245bb130b1391e6a52a7d449f
kfei <kfei@tencent.com>
2024-04-27 20:49:10 +0800

    1

--- scripts/descm_res_train.sh
+++ scripts/descm_res_train.sh
@@ -27,7 +27,7 @@ nohup python src/train.py \
 model._target_=src.models.descm_res.MultiTaskLitModel_Res \
 model.model._target_=src.models.descm_res.DESCM_Embedding_Res_Simple \
 model.loss._target_=src.models.descm_res.Basic_Loss_Res_Ratio \
-+model.loss.trade_off_ctr_loss_0=1,0.1,0.3,0.5,0.8 \
-trainer.max_epochs=1,2,3 trainer.min_epochs=1 \
++model.loss.trade_off_ctr_loss_0=0.1,0.2,0.3,0.5,0.8,0.9 \
+trainer.max_epochs=1 trainer.min_epochs=1 \
 experiment=descm_res_experiment \
 test=True data.debug=False >logs/out.log 2>&1 & \
