22ae7008719a6844ee41f46ec12e54e6352834b6
Your Name <you@example.com>
2024-04-28 08:38:32 +0800

    1

--- scripts/descm_res_train.sh
+++ scripts/descm_res_train.sh
@@ -12,12 +12,11 @@ test=True data.debug=True >logs/out.log 2>&1 & \
 
 # simple ratio 训练
 nohup python src/train.py \
--m data=ccp,fr,us,nl,es \
+-m data=ccp,nl,us,es \
 model._target_=src.models.descm_res.MultiTaskLitModel_Res \
-model.model._target_=src.models.descm_res.DESCM_Embedding_Res_Simple \
-model.loss._target_=src.models.descm_res.Basic_Loss_Res_Ratio \
-+model.loss.trade_off_ctr_loss_0=0.1,0.3,0.5,0.8,1.5,2,2.5 \
-trainer.max_epochs=3 trainer.min_epochs=3 \
+model.model._target_=src.models.descm_res.DESCM_Embedding_Res_M0,src.models.descm_res.DESCM_Embedding_Res_M1 \
+model.loss._target_=src.models.descm_res.Basic_Loss_Res \
+trainer.max_epochs=3,4,5 trainer.min_epochs=1 \
 experiment=descm_res_experiment \
 test=True data.debug=False >logs/out.log 2>&1 & \
 
