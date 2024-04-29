ddf90aa7077afd240215733ee6098425d9f1c613
Your Name <you@example.com>
2024-04-29 11:44:05 +0800

    1

--- scripts/descm_res_train.sh
+++ scripts/descm_res_train.sh
@@ -12,11 +12,12 @@ test=True data.debug=True >logs/out.log 2>&1 & \
 
 # simple 训练
 nohup python src/train.py \
--m data=ccp,fr,nl,us,es \
+-m data=ccp \
 model._target_=src.models.descm_res.MultiTaskLitModel_Res \
 model.model._target_=src.models.descm_res.DESCM_Embedding_Res_Cross1 \
-model.loss._target_=src.models.descm_res.Basic_Loss_Res \
-+model.model.cross_num=1,2,3 \
+model.loss._target_=src.models.descm_res.Basic_Loss_Res_Unclick \
++model.model.cross_num=1 \
++model.loss.trade_off_unclick_loss=0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9 \
 trainer.max_epochs=3 trainer.min_epochs=1 \
 experiment=descm_res_experiment \
 test=True data.debug=False >logs/out.log 2>&1 & \
@@ -25,9 +26,10 @@ test=True data.debug=False >logs/out.log 2>&1 & \
 nohup python src/train.py \
 -m data=in \
 model._target_=src.models.descm_res.MultiTaskLitModel_Res \
-model.model._target_=src.models.descm_res.DESCM_Embedding_Res_Simple \
+model.model._target_=src.models.descm_res.DESCM_Embedding_Res_Cross1 \
 model.loss._target_=src.models.descm_res.Basic_Loss_Res_Unclick \
-+model.loss.trade_off_unclick_loss=0.1,0.5,1,2,5,10,100 \
++model.model.cross_num=1 \
++model.loss.trade_off_unclick_loss=0.1 \
 trainer.max_epochs=1 trainer.min_epochs=1 \
 data.batch_size=8000 \
 experiment=descm_res_experiment \
