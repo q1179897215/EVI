63143f7436cb16a43d4c5df64b6ebdede2fe1bbd
Your Name <you@example.com>
2024-07-26 20:52:17 +0800

    1

---
+++ scripts/descm_ukd_train.sh
@@ -0,0 +1,13 @@
+# single
+# python  src/train.py -m  data=ccp model.model._target_=src.models.descm.ESCM,src.models.descm.DESCM_Embedding experiment=descm_experiment test=False data.debug=True logger.wandb.project=lightning-hydra-template
+
+
+# grid search 
+# nohup python src/train.py -m data=ccp,fr,us,nl,es model.model._target_=src.models.descm.DESCM_Embedding_DA_1_1 model.loss._target_=src.models.common.Basic_Loss model._target_=src.models.descm.MultiTaskLitModel_DA_click_to_impression experiment=descm_experiment test=True data.debug=True >logs/out.log 2>&1 &
+
+# nohup python src/train.py -m data=ccp,fr,us,nl,es model.loss._target_=src.models.common.Basic_Loss,src.models.common.Impression_CTR_IPW_Loss,src.models.common.Unclick_CTR_IPW_Loss,src.models.common.Click_CTR_IPW_Loss  experiment=descm_experiment test=True data.debug=False >logs/out.log 2>&1 &
+
+python src/train.py -m data=ccp experiment=descm_ukd_experiment test=True data.debug=False trainer.max_epochs=1 trainer.min_epochs=1
+
+python src/train.py -m data=in experiment=descm_ukd_experiment test=True data.debug=False trainer.max_epochs=1 trainer.min_epochs=1
+
